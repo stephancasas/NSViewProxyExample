e3f015d86aed65f9530367392387849cfc690fdb
Stephan Casas <stephancasas@icloud.com>
2023-06-15 19:30:02 -0500

    Initial Commit

---
+++ NSViewProxyExample/NSViewProxyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSViewProxyExampleApp.swift
+//  NSViewProxyExample
+//
+//  Created by Stephan Casas on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NSViewProxyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
